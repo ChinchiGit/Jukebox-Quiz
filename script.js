405a46eaaf9965e7c5aec4d16509189d123c7ade
unknown <you@example.com>
2024-04-09 16:11:11 +0200

    feat: Connect to Firestore for game scores storage and chart population

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js";
-import { getFirestore, collection, doc, getDoc, setDoc, updateDoc } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js";
+import { getFirestore, collection, where, getDocs, doc, setDoc, updateDoc, query, orderBy, limit, getDoc } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-storage.js";
 //TODO: Add SDKs for Firebase products that you want to use
 //https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,12 +9,13 @@ import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstati
 
 //CONECTAR FIREBASE
 const firebaseConfig = {
-  apiKey: "AIzaSyBpkER-BUrmgpsetSxLVGzIrL5TIc7kg5w",
-  authDomain: "prueba-autenticacion-y-mas.firebaseapp.com",
-  projectId: "prueba-autenticacion-y-mas",
-  storageBucket: "prueba-autenticacion-y-mas.appspot.com",
-  messagingSenderId: "666047643817",
-  appId: "1:666047643817:web:8e83a258e9c2867b3329b8"
+  apiKey: "AIzaSyBBySJ7kyqqwkhoo2dJFZDPa0XAgh4QoY8",
+  authDomain: "quiz-2-cea59.firebaseapp.com",
+  projectId: "quiz-2-cea59",
+  storageBucket: "quiz-2-cea59.appspot.com",
+  messagingSenderId: "812092197244",
+  appId: "1:812092197244:web:a0490411e36860e77ebcf9",
+  measurementId: "G-82J05KL4T3"
 };
 
 //Initialize Firebase
@@ -31,10 +32,11 @@ const storage = getStorage();
 //VARIABLES
 let preguntas = [];
 let correctas = [];
+let userName = "";
 let page = 0;
 let score = 0;
 let fecha = new Date;
-fecha = fecha.toDateString();
+fecha = fecha.toLocaleDateString();
 
 
 
@@ -57,18 +59,18 @@ if (document.title == "Jukebox Quiz - Home") {
 
     let signUpEmail = document.getElementById('signup-email').value;
 
-    //almacenamos ese email en LOCALSTORAGE
-    let guardar = JSON.parse(localStorage.getItem("mails"));
-    let mail = [{ email: signUpEmail }];
+    // //almacenamos ese email en LOCALSTORAGE
+    // let guardar = JSON.parse(localStorage.getItem("mails"));
+    // let mail = [{ email: signUpEmail }];
 
-    if (guardar === null) {
-      localStorage.setItem("mails", JSON.stringify(mail));
-    } else {
-      let newEmail = { email: signUpEmail };
+    // if (guardar === null) {
+    //   localStorage.setItem("mails", JSON.stringify(mail));
+    // } else {
+    //   let newEmail = { email: signUpEmail };
 
-      guardar.push(newEmail);
-      localStorage.setItem("mails", JSON.stringify(guardar));
-    }
+    //   guardar.push(newEmail);
+    //   localStorage.setItem("mails", JSON.stringify(guardar));
+    // }
 
 
     const signUpPassword = document.getElementById('signup-pass').value;
@@ -112,22 +114,6 @@ if (document.title == "Jukebox Quiz - Home") {
     loginEmail = document.getElementById('login-email').value;
 
 
-
-    //almacenamos ese mail en LOCAL STORAGE
-    let guardar = JSON.parse(localStorage.getItem("mails"));
-    let mail = [{ email: loginEmail }];
-
-    if (guardar === null) {
-      localStorage.setItem("mails", JSON.stringify(mail));
-    } else {
-      let newEmail = { email: loginEmail };
-
-      guardar.push(newEmail);
-      localStorage.setItem("mails", JSON.stringify(guardar));
-    }
-
-
-
     const loginPassword = document.getElementById('login-pass').value;
     let userData = document.getElementById('user-data');
     console.log(userData);
@@ -145,6 +131,7 @@ if (document.title == "Jukebox Quiz - Home") {
       .then(() => {
 
         if (docSnap.exists()) {
+          userName = docSnap.data().username;
           userData.style.cssText = 'display: block; background-color: #ffcfd2; width: 75%; margin: auto; padding: 5px;';
           userData.innerHTML = `<h3>Welcome!</h3>
                               <p>Username: ${docSnap.data().username}</p>
@@ -157,52 +144,94 @@ if (document.title == "Jukebox Quiz - Home") {
           document.getElementById("login-form").style.display = "none";
           document.getElementById("scoresBox").style.display = "block";
 
+          //almacenamos ese mail en LOCAL STORAGE
+          let guardar = JSON.parse(localStorage.getItem("mails"));
+          let mail = [{ email: loginEmail, username: userName }];
+
+          if (guardar === null) {
+            localStorage.setItem("mails", JSON.stringify(mail));
+          } else {
+            let newEmail = { email: loginEmail, username: userName };
+
+            guardar.push(newEmail);
+            localStorage.setItem("mails", JSON.stringify(guardar));
+          }
+
+
+
+
           //GRAFICA USUARIO LOGUEADO
 
 
-          let localData = JSON.parse(localStorage.getItem('mails'));
-          console.log(localData);
+          // let localData = JSON.parse(localStorage.getItem('mails'));
+          // console.log(localData);
           let fechasGrafica = [];
           let scoreGrafica = [];
-          for (let i = 0; i < localData.length; i++) {
-            if (localData[i]["email"] == loginEmail) {
-              fechasGrafica.push(localData[i]["fecha"]);
-              scoreGrafica.push(localData[i]["score"]);
-            };
+          // for (let i = 0; i < localData.length; i++) {
+          //   if (localData[i]["email"] == loginEmail) {
+          //     fechasGrafica.push(localData[i]["fecha"]);
+          //     scoreGrafica.push(localData[i]["score"]);
+          //   };
 
-          }
+          // }
 
-          for (let j = 0; j < scoreGrafica; j++) {
-            scoreGrafica[j].split(",", 4);
-          };
-
-
-
-          console.log(fechasGrafica, scoreGrafica);
+          // for (let j = 0; j < scoreGrafica; j++) {
+          //   scoreGrafica[j].split(",", 4);
+          // };
 
 
+          // Initialize Firestore
+          const db = getFirestore();
 
-          const ctx = document.getElementById('myChart');
-          new Chart(ctx, {
-            type: 'bar',
-            data: {
-              labels: [`${fechasGrafica[0]}`, `${fechasGrafica[1]}`, `${fechasGrafica[2]}`, `${fechasGrafica[3]}`],
-              datasets: [{
-                label: 'Correct Answers',
-                data: [`${scoreGrafica[0]}`, `${scoreGrafica[1]}`, `${scoreGrafica[2]}`, `${scoreGrafica[3]}`],
-                borderWidth: 1
-              }]
-            },
-            options: {
-              responsive: true,
-              aspectRatio: 1 | 1,
-              scales: {
-                y: {
-                  beginAtZero: true
+          // Reference to the "partidas" collection
+          let partidasRef = collection(db, "partidas");
+
+          // Query to get documents where "email" is equal to the logged in user's email
+          let q = query(partidasRef, where("email", "==", loginEmail));
+
+          // Execute the query
+          getDocs(q).then((querySnapshot) => {
+            console.log(q);
+            querySnapshot.forEach((doc) => {
+              console.log(doc.id, " => ", doc.data());
+              // Extract date and score and add them to fechasGrafica and scoreGrafica
+              fechasGrafica.push(doc.data().date);
+              scoreGrafica.push(doc.data().scores);
+            });
+
+            console.log(fechasGrafica, scoreGrafica);
+
+
+
+            const ctx = document.getElementById('myChart');
+            new Chart(ctx, {
+              type: 'bar',
+              data: {
+                labels: [`${fechasGrafica[0]}`, `${fechasGrafica[1]}`, `${fechasGrafica[2]}`, `${fechasGrafica[3]}`],
+                datasets: [{
+                  label: 'Correct Answers',
+                  data: [`${scoreGrafica[0]}`, `${scoreGrafica[1]}`, `${scoreGrafica[2]}`, `${scoreGrafica[3]}`],
+                  borderWidth: 1
+                }]
+              },
+              options: {
+                responsive: true,
+                aspectRatio: 1 | 1,
+                scales: {
+                  y: {
+                    beginAtZero: true
+                  }
                 }
               }
-            }
+            });
+
           });
+
+
+
+
+
+
 
         } else {
           console.log("No such document!");
@@ -261,10 +290,23 @@ function submitForm() {
 
     let playersMails = JSON.parse(localStorage.getItem("mails"));
     let ultimo = playersMails[playersMails.length - 1];
+
+    let playerUserName = [];
+    const user = auth.currentUser;
+    console.log("Objeto user: ", user)
+    const userRef = doc(db, 'users', user.email);
+    const userDoc = await getDoc(userRef);
+    if (userDoc.exists()) {
+      playerUserName.push(userDoc.data().username);
+      console.log("Esto es lo que tratamos de guardar: ", playerUserName)
+    }
     let fechaScore = {
       fecha: fecha,
       score: score
     }
+
+
+
     const character = Object.assign(ultimo, fechaScore);
 
     playersMails[playersMails.length - 1] = character;
@@ -278,7 +320,8 @@ function submitForm() {
     const docRef = doc(db, "partidas", id);
 
     const data = {
-      email: ultimo,
+      email: ultimo.email,
+      username: ultimo.username,
       date: fecha,
       scores: score
     };
@@ -376,11 +419,11 @@ if (document.title === 'Jukebox Quiz - Results') {
   let lastround = local[local.length - 1].score;
   console.log(lastround)
 
-for (let x =0; x<local.length ; x++){
-  if (local[x].score == undefined){ 
-  local.splice(x, 1)
+  for (let x = 0; x < local.length; x++) {
+    if (local[x].score == undefined) {
+      local.splice(x, 1)
+    }
   }
-}
 
   let meterPuntuacion = document.getElementById("meterPuntuacion");
   meterPuntuacion.innerHTML = `You have answered ${lastround} questions correctly`
@@ -390,43 +433,70 @@ for (let x =0; x<local.length ; x++){
   <h2 class="fontResult">Hall of Fame: </h2>`
 
 
-  local.sort(function (a, b) {
-    if (a["score"] > b["score"]) {
-      return 1;
-    }
-    if (a["score"] < b["score"]) {
-      return -1;
-    }
+  //   local.sort(function (a, b) {
+  //     if (a["score"] > b["score"]) {
+  //       return 1;
+  //     }
+  //     if (a["score"] < b["score"]) {
+  //       return -1;
+  //     }
 
-    return 0;
+  //     return 0;
+  //   });
+
+  //   local = local.slice(0, 5);
+
+  //   const table = document.getElementById('table')
+  //   table.innerHTML = `                      
+  //                       <table>
+  //                         <thead>
+  //                           <tr>
+  //                             <th>Player</th>
+  //                             <th>Date</th>
+  //                             <th>Result</th>
+  //                           </tr>
+  //                         </thead>
+  //                         <tbody>
+  //                           ${local.map((local) => `
+  //                             <tr>
+  //                               <td>${local.email}</td>
+  //                               <td>${local.fecha}</td>
+  //                               <td>${local.score}</td>
+  //                             </tr>
+  //                           `).join("")}
+  //                         </tbody>
+  //                       </table>
+  //                     `;
+
+  // }
+
+
+  const partidasRef = collection(db, 'partidas');
+  const peticion = query(partidasRef, orderBy('scores', 'desc'), limit(5));
+  getDocs(peticion).then((querySnapshot) => {
+    const table = document.getElementById('table');
+    table.innerHTML = `
+      <table>
+        <thead>
+          <tr>
+            <th>Player</th>
+            <th>Date</th>
+            <th>Result</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${querySnapshot.docs.map((doc) => `
+            <tr>
+              <td>${doc.data().username}</td>
+              <td>${doc.data().date}</td>
+              <td>${doc.data().scores}" of 10"</td>
+            </tr>
+          `).join('')}
+        </tbody>
+      </table>
+    `;
   });
-
-  local = local.slice(0, 5);
-
-  const table = document.getElementById('table')
-  table.innerHTML = `                      
-                      <table>
-                        <thead>
-                          <tr>
-                            <th>Player</th>
-                            <th>Date</th>
-                            <th>Result</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          ${local.map((local) => `
-                            <tr>
-                              <td>${local.email}</td>
-                              <td>${local.fecha}</td>
-                              <td>${local.score}</td>
-                            </tr>
-                          `).join("")}
-                        </tbody>
-                      </table>
-                    `;
-
 }
-
 
 
 //// ______________ BOTONES _____________________
